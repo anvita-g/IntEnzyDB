c5927c847ff2e77872d2ffbb5d36726ede53e898
Anvita Gollu <75912590+anvita-g@users.noreply.github.com>
2022-05-24 15:46:39 -0400

    Update enzydb.js

--- controllers/enzydb.js
+++ controllers/enzydb.js
@@ -6,6 +6,9 @@ const application  = express();
 
 //const KineticsModel = mongoose.model("hydrolases_kinetics")
 const KineticsModel = mongoose.model("kinetics_table")
+const StructureModel = mongoose.model("structure_chain_level_table")
+const ReferenceModel = mongoose.model("reference_table")
+
 
 router.get("/list",(req, res)=>{
     mongoose.pluralize(null);
@@ -79,6 +82,116 @@ router.get("/list/searchUNIPROT",(req, res)=>{
     var myquery = {'uniprot': resN};
 
     KineticsModel.find(myquery, function(err, document){
+        if (!err){
+            res.render('list', {data : document});
+        }
+        else {
+            const msg = "Error while searching.";
+            res.render('message', {data : msg});
+        }
+    }).lean();
+});
+
+router.get("/structure",(req, res)=>{
+    mongoose.pluralize(null);
+    StructureModel.find((err, docs) =>{
+        console.log("no error")
+        if (!err){
+         //   console.log(docs)
+            res.render('structure', {data : docs})
+
+        }
+    }).lean();
+});
+
+router.get("/reference",(req, res)=>{
+    mongoose.pluralize(null);
+    ReferenceModel.find((err, docs) =>{
+        console.log("no error")
+        if (!err){
+            //   console.log(docs)
+            res.render('reference', {data : docs})
+
+        }
+    }).lean();
+});
+
+
+router.get("/structure/searchEC",(req, res)=>{
+    var ObjectId = require('mongodb').ObjectID;
+    const { resN } = req.query;
+    console.log(resN);
+    var myquery = {'Ec_Number': resN};
+
+    StructureModel.find(myquery, function(err, document){
+        if (!err){
+            res.render('list', {data : document});
+        }
+        else {
+            const msg = "Error while searching.";
+            res.render('message', {data : msg});
+        }
+    }).lean();
+});
+
+router.get("/structure/searchPDBID",(req, res)=>{
+    var ObjectId = require('mongodb').ObjectID;
+    const { resN } = req.query;
+    console.log(resN);
+    var myquery = {'PDB_ID': resN};
+
+    StructureModel.find(myquery, function(err, document){
+        if (!err){
+            res.render('list', {data : document});
+        }
+        else {
+            const msg = "Error while searching.";
+            res.render('message', {data : msg});
+        }
+    }).lean();
+});
+
+router.get("/reference/searchEC",(req, res)=>{
+    var ObjectId = require('mongodb').ObjectID;
+    const { resN } = req.query;
+    console.log(resN);
+    var myquery = {'ec_number': resN};
+
+    ReferenceModel.find(myquery, function(err, document){
+        if (!err){
+            res.render('list', {data : document});
+        }
+        else {
+            const msg = "Error while searching.";
+            res.render('message', {data : msg});
+        }
+    }).lean();
+});
+
+router.get("/reference/searchPDBID",(req, res)=>{
+    var ObjectId = require('mongodb').ObjectID;
+    const { resN } = req.query;
+    console.log(resN);
+    var myquery = {'PDB_ID': resN};
+
+    ReferenceModel.find(myquery, function(err, document){
+        if (!err){
+            res.render('list', {data : document});
+        }
+        else {
+            const msg = "Error while searching.";
+            res.render('message', {data : msg});
+        }
+    }).lean();
+});
+
+router.get("/reference/searchUNIPROT",(req, res)=>{
+    var ObjectId = require('mongodb').ObjectID;
+    const { resN } = req.query;
+    console.log(resN);
+    var myquery = {'uniprot': resN};
+
+    ReferenceModel.find(myquery, function(err, document){
         if (!err){
             res.render('list', {data : document});
         }
