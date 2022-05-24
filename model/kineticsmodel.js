0b24ec222ff7bff976266e259df3b111f8046ebb
Anvita Gollu <75912590+anvita-g@users.noreply.github.com>
2022-05-24 15:46:19 -0400

    Update kineticsmodel.js

--- model/kineticsmodel.js
+++ model/kineticsmodel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const {watchOptions} = require("nodemon/lib/config/defaults");
 
 /* var KineticsSchema = new mongoose.Schema({
     ec_number : String,
@@ -51,5 +52,51 @@ var KineticsTableSchema = new mongoose.Schema({
     collection:"kinetics_table"
 });
 
+var StructuresTableSchema = new mongoose.Schema({
+    PDB_ID: String,
+    Enzyme_Type: String,
+    Name: String,
+    Mutation: String,
+    Global_Stoichiometry: String,
+    Organism_Name: String,
+    Organism_Taxid: String,
+    Ec_Number: String,
+    Strain: String,
+    Gene: String,
+    Resolution: String,
+    Chain_Id: String,
+    Fasta_Sequence: String,
+    Partial_Modeled_Residue: String,
+    Unmodeled_Residue: String,
+    Active_Site: String,
+    Entity_ID: String,
+    Num_Seqres: String,
+    Seqres: String
+
+}, {
+    collection:"structure_chain_level_table"
+});
+
+var ReferencesTableSchema = new mongoose.Schema({
+    PDB_ID: String,
+    uniprot: String,
+    chainID: String,
+    Active_Site: String,
+    chain_n_res: String,
+    Resolution: String,
+    Resolution_n: String,
+    reso_filter: String,
+    pdb_chain: String,
+    auth_as: String,
+    ec_number: String,
+    PDB_website: String,
+    uniprot_webstie: String
+
+}, {
+    collection:"reference_table"
+});
+
 //mongoose.model("hydrolases_kinetics",KineticsSchema);
 mongoose.model("kinetics_table",KineticsTableSchema);
+mongoose.model("structure_chain_level_table",StructuresTableSchema);
+mongoose.model("reference_table",ReferencesTableSchema);
